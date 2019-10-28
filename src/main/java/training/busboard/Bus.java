6b6a8173823f5c29e9aae3bb7842311581884c6b
Kapil Krishna <Kapil.Krishna@softwire.com>
2019-10-28 16:21:01 +0000

    Finished Part 1

---
+++ src/main/java/training/busboard/Bus.java
@@ -0,0 +1,38 @@
+package training.busboard;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties (ignoreUnknown = true)
+
+public class Bus {
+    private String id;
+    private String lineName;
+    private String destinationName;
+    private Integer timeToStation;
+    private String expectedArrival;
+
+    @Override
+    public String toString () {
+        String print = String.format("|%-5s|%-25s|%-5d|%-20s", lineName, destinationName, timeToStation, expectedArrival);
+        return print;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getLineName() {
+        return lineName;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public Integer getTimeToStation() {
+        return timeToStation;
+    }
+
+    public String getExpectedArrival() {
+        return expectedArrival;
+    }
+}
--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,7 +1,43 @@
 package training.busboard;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.xml.transform.Source;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String args[]) {
-        // Your code here!
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the Bus Stop finder! Input your Bus Stop Code to find out the next 5 buses" +
+                        " arriving at your stop!");
+        String stopCode = input.nextLine();
+
+//        Pattern pattern = Pattern.compile("[\\w]{9,11}");
+//        Matcher matcher = pattern.matcher(stopCode);
+//
+//        while(!(matcher.matches())) {
+//            Scanner repeat = new Scanner(System.in);
+//            System.out.println("That's not a valid Stop Code. Try again.");
+//            stopCode = repeat.nextLine();
+//        }
+
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+        List<Bus> busList = client.target("" +
+                "https://api.tfl.gov.uk/StopPoint/" + stopCode + "/Arrivals?app_id=da4a2af3&app_key=98ba2b65d63f9c812a52c6c095eced7a")
+                .request(MediaType.APPLICATION_JSON)
+                .get(new GenericType<List<Bus>>() {});
+
+        for (int i = 1; i <= 5 && i < (busList.size()); i++) {
+            System.out.println(busList.get(i));
+        }
+
+//        System.out.println(busList);
     }
 }	
